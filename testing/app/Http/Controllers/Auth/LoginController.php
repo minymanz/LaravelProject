a90d74038405be5cc6f0941a1e9d26309add5a67
minymanz <afifhan95@gmail.com>
2019-07-31 19:53:22 +0700

    InitialCommit testing : first project for vagrant and homestead training

---
+++ testing/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Role;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    //protected $redirectTo = '/home';
+    public function redirectTo(){
+        
+        // User role
+        $id = Auth::user()->id;
+        $role = User::find($id)->getRoleNames();
+
+        // Check user role
+        switch ($role[0]) {
+            case 'Super Admin':
+                    return 'users';
+                break;
+            case 'Admin':
+                    return 'users';
+                break; 
+            default:
+                    return 'products'; 
+                break;
+        }
+    }
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+}
